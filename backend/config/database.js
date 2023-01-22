018706ac499b32c2b6cac775fdf9c4a9f7d58032
Jacob North <yakenorth@protonmail.com>
2023-01-21 23:42:13 -0500

    added .env.example and reworked config files

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,19 @@
 const config = require("./index");
 
+const db = config.db;
+const username = db.username;
+const password = db.password;
+const database = db.database;
+const host = db.host;
+
 module.exports = {
 	development: {
-		storage: config.dbFile,
-		dialect: "sqlite",
+		username,
+		password,
+		database,
+		host,
+		dialect: "postgres",
 		seederStorage: "sequelize",
-		logQueryParameters: true,
-		typeValidation: true,
 	},
 	production: {
 		use_env_variable: "DATABASE_URL",
